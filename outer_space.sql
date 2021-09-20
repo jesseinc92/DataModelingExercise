fb297ded038eeb7944305efc0a1b8f21f031133e
Jesse Hill <jesse.o.hill@gmail.com>
2021-09-20 06:46:12 -0400

    implemented 3 SQL schema updates

---
+++ outer_space.sql
@@ -0,0 +1,79 @@
+-- from the terminal run:
+-- psql < outer_space.sql
+
+DROP DATABASE IF EXISTS outer_space;
+
+CREATE DATABASE outer_space;
+
+\c outer_space
+
+CREATE TABLE suns
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE planets
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around INT REFERENCES suns,
+  galaxy INT REFERENCES galaxies
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  orbits_around INT REFERENCES planets
+);
+
+INSERT INTO suns
+  (name)
+VALUES
+  ('The Sun'),
+  ('Proxima Centauri'),
+  ('Gliese 876');
+
+INSERT INTO galaxies
+  (name)
+VALUES
+  ('Milky Way');
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbits_around, galaxy)
+VALUES
+  ('Earth', 1.00, 1, 1),
+  ('Mars', 1.88, 1, 1),
+  ('Venus', 0.62, 1, 1),
+  ('Neptune', 164.8, 1, 1),
+  ('Proxima Cenauri b', 0.03, 2, 1),
+  ('Gliese 876 b', 0.23, 3, 1);
+
+INSERT INTO moons
+  (name, orbits_around)
+VALUES
+  ('The Moon', 1),
+  ('Phobos', 2),
+  ('Deimos', 2),
+  ('Naiad', 4),
+  ('Thalassa', 4),
+  ('Despina', 4),
+  ('Galatea', 4),
+  ('Larissa', 4),
+  ('S/2004 N 1', 4),
+  ('Proteus', 4),
+  ('Trion', 4),
+  ('Nereid', 4),
+  ('Halimede', 4),
+  ('Sao', 4),
+  ('Laomedeia', 4),
+  ('Psamathe', 4),
+  ('Neso', 4);
